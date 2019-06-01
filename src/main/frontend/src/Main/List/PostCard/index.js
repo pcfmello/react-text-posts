01d83ce08a920705c34855f0edffc9689c7497f1
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-06-01 17:15:28 -0300

    Create handleUpVote funcion and refactoring code - Creating handleUpVote to user voting on posts;

--- src/main/frontend/src/Main/List/PostCard/index.js
+++ src/main/frontend/src/Main/List/PostCard/index.js
@@ -5,91 +5,93 @@ import moment from "moment";
 import "moment/locale/pt-br";
 
 import {
-  makeStyles,
-  Card,
-  CardHeader,
-  CardContent,
-  CardActions,
-  Avatar,
-  IconButton,
-  Typography
+    makeStyles,
+    Card,
+    CardHeader,
+    CardContent,
+    CardActions,
+    Avatar,
+    IconButton,
+    Typography
 } from "@material-ui/core";
 import { grey, red } from "@material-ui/core/colors";
 import { Favorite } from "@material-ui/icons";
 
 const useStyles = makeStyles(theme => ({
-  card: {
-    marginBottom: theme.spacing(1)
-  },
-  media: {
-    height: 0,
-    paddingTop: "56.25%" // 16:9
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest
-    })
-  },
-  expandOpen: {
-    transform: "rotate(180deg)"
-  },
-  avatar: {
-    backgroundColor: grey[600]
-  },
-  postIsVoted: {
-    color: red[500]
-  },
-  postIsNotVoted: {
-    color: grey[600]
-  }
+    card: {
+        marginBottom: theme.spacing(1)
+    },
+    media: {
+        height: 0,
+        paddingTop: "56.25%" // 16:9
+    },
+    expand: {
+        transform: "rotate(0deg)",
+        marginLeft: "auto",
+        transition: theme.transitions.create("transform", {
+            duration: theme.transitions.duration.shortest
+        })
+    },
+    expandOpen: {
+        transform: "rotate(180deg)"
+    },
+    avatar: {
+        backgroundColor: grey[600]
+    },
+    postIsVoted: {
+        color: red[500]
+    },
+    postIsNotVoted: {
+        color: grey[600]
+    }
 }));
 
-const PostCard = ({ post }) => {
-  const classes = useStyles();
-  const [expanded, setExpanded] = useState(false);
+const PostCard = ({ post, handleUpVote }) => {
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
 
-  const handleExpandClick = () => setExpanded(!expanded);
+    const handleExpandClick = () => setExpanded(!expanded);
 
-  const vote = async () => {
-    try {
-      const postChanged = await API.put(`/post/${post.id}`);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  return (
-    <Card className={classes.card}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="Recipe" className={classes.avatar}>
-            {post.owner.substring(0, 1).toUpperCase()}
-          </Avatar>
+    const vote = async () => {
+        try {
+            await API.put(`/post/${post.id}`);
+            handleUpVote(post.id);
+        } catch (error) {
+            console.log(error.message);
         }
-        title={post.owner}
-        subheader={moment(post.postDate).format("LL")}
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {post.message}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="Up vote" onClick={vote}>
-          <Favorite className={classes.favoriteIcon} />
-        </IconButton>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {post.upVotes} up votes
-        </Typography>
-      </CardActions>
-    </Card>
-  );
+    };
+
+    return (
+        <Card className={classes.card}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="Recipe" className={classes.avatar}>
+                        {post.owner.substring(0, 1).toUpperCase()}
+                    </Avatar>
+                }
+                title={post.owner}
+                subheader={moment(post.postDate).format("LL")}
+            />
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {post.message}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="Up vote" onClick={vote}>
+                    <Favorite />
+                </IconButton>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {post.upVotes} up votes
+                </Typography>
+            </CardActions>
+        </Card>
+    );
 };
 
 PostCard.propTypes = {
-  post: PropTypes.object.isRequired
+    post: PropTypes.object.isRequired,
+    handleUpVote: PropTypes.func.isRequired
 };
 
 export default PostCard;
--- src/main/frontend/src/Main/List/index.js
+++ src/main/frontend/src/Main/List/index.js
@@ -7,64 +7,94 @@ import { Add, Reorder } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-  footer: {
-    margin: theme.spacing(2),
-    position: "fixed",
-    bottom: 0,
-    right: 0
-  }
+    footer: {
+        margin: theme.spacing(2),
+        position: "fixed",
+        bottom: 0,
+        right: 0
+    },
+    addIconButton: {
+        color: "white",
+        backgroundColor: "#282c34"
+    },
+    emptyList: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    emptyListIcon: {
+        color: "rgba(0, 0, 0, 0.54)",
+        fontSize: "7em"
+    },
+    emptyListText: {
+        fontSize: "1.6em"
+    }
 }));
 
 const List = () => {
-  const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    getPosts();
-  }, []);
+    useEffect(() => {
+        getPosts();
+    }, []);
 
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const getPosts = async () => {
-    try {
-      const response = await API.get("/posts");
-      setPosts(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const getPosts = async () => {
+        try {
+            setIsLoading(true);
+            const response = await API.get("/posts");
+            setPosts(response.data);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
-  return (
-    <React.Fragment>
-      {!!posts.length &&
-        posts.map(post => <PostCard key={post.id} {...{ post }} />)}
-      {!posts.length && (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center"
-          }}
-        >
-          <Reorder style={{ color: "rgba(0, 0, 0, 0.54)", fontSize: "7em" }} />
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="p"
-            style={{ fontSize: "1.6em" }}
-          >
-            Ainda não existem posts
-          </Typography>
-        </div>
-      )}
-      <div className={classes.footer}>
-        <Link to="/add">
-          <Fab color="primary" aria-label="Add" className={classes.fab}>
-            <Add />
-          </Fab>
-        </Link>
-      </div>
-    </React.Fragment>
-  );
+    const handleUpVote = postId => {
+        const newPosts = posts.map(post =>
+            post.id === postId ? { ...post, upVotes: post.upVotes + 1 } : post
+        );
+        setPosts(newPosts);
+    };
+
+    return (
+        <React.Fragment>
+            {!isLoading && (
+                <div>
+                    {!!posts.length &&
+                        posts.map(post => (
+                            <PostCard
+                                key={post.id}
+                                {...{ post, handleUpVote }}
+                            />
+                        ))}
+                    {!posts.length && (
+                        <div className={classes.emptyList}>
+                            <Reorder className={classes.emptyListIcon} />
+                            <Typography
+                                className={classes.emptyListText}
+                                variant="body2"
+                                color="textSecondary"
+                                component="p"
+                            >
+                                Ainda não existem posts
+                            </Typography>
+                        </div>
+                    )}
+                </div>
+            )}
+            <div className={classes.footer}>
+                <Link to="/add">
+                    <Fab className={classes.addIconButton} aria-label="Add">
+                        <Add />
+                    </Fab>
+                </Link>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default List;
