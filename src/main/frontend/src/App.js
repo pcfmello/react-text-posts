bda1eaa39297c95ba48a6541b0fb6c8d7eb0dec2
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-06-02 17:56:00 -0300

    Changed place header component and react router

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./Header";
 import Main from "./Main";
 import { BrowserRouter } from "react-router-dom";
 
@@ -7,6 +8,7 @@ import "./App.scss";
 const App = () => (
   <div className="App">
     <BrowserRouter>
+      <Header />
       <Main />
     </BrowserRouter>
   </div>
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -2,19 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import Header from "./Header";
 import App from "./App";
 
 import * as serviceWorker from "./serviceWorker";
 import "typeface-roboto";
 
-ReactDOM.render(
-  <React.Fragment>
-    <Header />
-    <App />
-  </React.Fragment>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
