47d19feba8e9234780647e90f11f880fc2760a79
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-06-02 20:16:39 -0300

    Rename components file name and include imports

---
+++ src/main/frontend/src/Main/Add/Add.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography
+} from "@material-ui/core";
+
+import Form from "./FormAdd";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    marginBottom: theme.spacing(1),
+
+    "& .card-actions": {
+      padding: theme.spacing(2),
+      paddingTop: 0
+    }
+  }
+}));
+
+const Add = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography
+          color="textSecondary"
+          gutterBottom
+          align="center"
+          variant="h6"
+        >
+          O que você gostaria postar?
+        </Typography>
+        <Form />
+      </CardContent>
+      <CardActions className="card-actions">
+        <Button
+          type="submit"
+          form="PostForm"
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          fullWidth
+          size="large"
+        >
+          OK
+        </Button>
+        <Button
+          variant="contained"
+          className={classes.button}
+          fullWidth
+          component={Link}
+          to="/"
+          size="large"
+        >
+          Cancelar
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Add;
---
+++ src/main/frontend/src/Main/List/PostCard/PostCard.js
@@ -0,0 +1,75 @@
+import React from "react";
+import PropTypes from "prop-types";
+import API from "../../../Api";
+import moment from "moment";
+import "moment/locale/pt-br";
+
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Avatar,
+  IconButton,
+  Typography
+} from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
+import { Favorite } from "@material-ui/icons";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    marginBottom: theme.spacing(1),
+
+    "& .avatar": {
+      backgroundColor: grey[600]
+    }
+  }
+}));
+
+const PostCard = ({ post, handleUpVote }) => {
+  const classes = useStyles();
+
+  const vote = async () => {
+    try {
+      await API.put(`/post/${post.id}`);
+      handleUpVote(post.id);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="Recipe" className="avatar">
+            {post.owner.substring(0, 1).toUpperCase()}
+          </Avatar>
+        }
+        title={post.owner}
+        subheader={moment(post.postDate).format("LL")}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="Up vote" onClick={vote}>
+          <Favorite />
+        </IconButton>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {post.upVotes} up votes
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.object.isRequired,
+  handleUpVote: PropTypes.func.isRequired
+};
+
+export default PostCard;
